f6e9bdf0152da82dba0268d64824675ed3eabd8d
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2022-12-30 18:03:17 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P122_W23_Lecture_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Back!");
+        } // Main
+
+        // Topics
+        // Review Class Policies
+        // Extra Credit Oppertunties
+        // GitHub Classrooms and GitHub workflow
+        // Programming 1 Review
+        // Discuss Homework
+
+    } // class
+
+} // namespace
---
+++ README.md
@@ -0,0 +1,2 @@
+# P122_W23_Lecture_1
+ Programming 122 Lecture 1 Notes - 
